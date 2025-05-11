d65004884d9dc7de3f2526e8c394567d33aa0652
07CalC <maheshwarivinayak90@gmail.com>
2025-05-11 19:40:47 +0530

    build setup

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,9 +1,11 @@
+import { FaAirbnb } from 'react-icons/fa';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="@cosmi-c/dashboard" />
+      <FaAirbnb className='text-4xl self-center'/>
     </div>
   );
 }
